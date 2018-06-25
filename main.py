57a093b6ea0bd4aeda08690a5f77966c0d5c758b
Ken <kentodd@frontier.com>
2018-06-24 18:28:33 -0700

    lesson1 activity ready to deploy...hahaha, I think

--- main.py
+++ main.py
@@ -11,8 +11,57 @@ app = Flask(__name__)
 
 @app.route('/all')
 def all_tasks():
- return render_template('all.jinja2', tasks=Task.select())
+    return render_template('all.jinja2', tasks=Task.select())
+
+
+@app.route('/create', methods=['GET', 'POST'])
+def create():
+
+    if request.method == 'POST':
+        task = Task(name=request.form['name'])
+        task.save()
+
+        return redirect(url_for('all_tasks'))
+    else:
+        return render_template('create.jinja2')
 
 if __name__ == "__main__":
  port = int(os.environ.get("PORT", 5000))
  app.run(host='0.0.0.0', port=port)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        user = User.select().where(User.name == request.form['name']).get()
+
+        if user and pbkdf2_sha256.verify(request.form['password'], user.password):
+            session['username'] = request.form['name']
+            return redirect(url_for('all_tasks'))
+
+        return render_template('login.jinja2', error="Incorrect username or password.")
+
+    else:
+        return render_template('login.jinja2')
+
+
+@app.route('/create', methods=['GET', 'POST'])
+def create():
+    if 'username' not in session:
+        return redirect(url_for('login'))
+
+
+@app.route('/incomplete', methods=['GET', 'POST'])
+@app.route('/incomplete', methods=['GET', 'POST'])
+def incomplete_tasks():
+    if 'username' not in session:
+        return redirect(url_for('login'))
+
+    if request.method == 'POST':
+        user = User.select().where(User.name == session['username']).get()
+
+        Task.update(performed=datetime.now(), performed_by=user)\
+            .where(Task.id == request.form['task_id'])\
+            .execute()
+
+    return render_template('incomplete.jinja2', tasks=Task.select().where(Task.performed.is_null()))
--- model.py
+++ model.py
@@ -7,14 +7,17 @@ db = connect(os.environ.get('DATABASE_URL', 'sqlite:///my_database.db'))
 
 
 class User(Model):
-    # TODO: Add model fields here
+    name = CharField(max_length=255, unique=True)
+    password = CharField(max_length=255)
 
     class Meta:
         database = db
 
 
 class Task(Model):
-    # TODO: Add model fields here
+    name = CharField(max_length=255)
+    performed = DateTimeField(null=True)
+    performed_by = ForeignKeyField(model=User, null=True)
 
     class Meta:
         database = db
